889ac7017c2000e36d5b715eb4c940986f655d8c
lichena <lichena@wharton.upenn.edu>
2020-02-10 18:32:17 -0500

    Updates

---
+++ precache-manifest.b95292606ef5be2638745015c1a18733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97346521630d17ff929adec8231a0044",
+    "url": "/nbd/index.html"
+  },
+  {
+    "revision": "6ab452bc0402429dcc3b",
+    "url": "/nbd/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5cd33154dd65c82cbbbe",
+    "url": "/nbd/static/js/2.83564051.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nbd/static/js/2.83564051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab452bc0402429dcc3b",
+    "url": "/nbd/static/js/main.144ec085.chunk.js"
+  },
+  {
+    "revision": "cb0bad1c1e31adfb1992",
+    "url": "/nbd/static/js/runtime-main.12943e15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nbd/static/media/logo.5d5d9eef.svg"
+  }
+]);
