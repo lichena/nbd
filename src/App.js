22689f7e9a828f7ff499571c6e32787d2e25154d
lichena <lichena@wharton.upenn.edu>
2020-02-12 14:03:11 -0500

    finished model

--- src/App.js
+++ src/App.js
@@ -2,25 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import EssayForm from './EssayForm'
+
+import {nbd} from './nbd'
+import Histogram from './Histogram';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  
+  onFormSubmit = (data) => {
+    let result = nbd(data, 'frequency');
+    if (result) {
+      this.setState({r: result.r, alpha: result.alpha, data: result.data, t: result.t, word_count: result.word_count})
+    }
+  }
+
+  renderHist = () => {
+    if (this.state.data && this.state.r && this.state.alpha && this.state.word_count && this.state.t) {
+      return <Histogram data={this.state.data} r={this.state.r} alpha={this.state.alpha} t={this.state.t} word_count={this.state.word_count}/>
+    }
+  }
+  render()  { 
+    return (
+      <div className="App">
+        <EssayForm onFormSubmit={this.onFormSubmit}/>
+        {this.renderHist()}
+        <p>{this.state.r ? 'r is '+ this.state.r.toFixed(3) : ''}</p>
+        <p>{this.state.alpha ? 'alpha is ' + this.state.alpha.toFixed(3) : ''}</p>
+        <p>{this.state.t ? 't is ' + this.state.t : ''}</p>
+        <p>{this.state.ll ? 'Log-Likelihood is ' + this.state.ll : ''}</p>
+        <p>{this.state.data ? 'number of unique words is ' + this.state.data.length : ''}</p>
+        <p>{this.state.word_count ? 'number of total words is ' + this.state.word_count : ''}</p>
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <EssayForm/>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
