8a545520b1208e72af51b0ac8b7f8102ae94baa7
lichena <lichena@wharton.upenn.edu>
2020-02-13 19:21:34 -0500

    Updates

---
+++ precache-manifest.19a16a0a2fa0b9d6f1f4953e432c471c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c15a2a789ee25199fa7b132ee77bad",
+    "url": "/nbd/index.html"
+  },
+  {
+    "revision": "7bbc666486d496785ffd",
+    "url": "/nbd/static/css/main.a425fc1e.chunk.css"
+  },
+  {
+    "revision": "ed404029081d0ef86122",
+    "url": "/nbd/static/js/2.6b52c191.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nbd/static/js/2.6b52c191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbc666486d496785ffd",
+    "url": "/nbd/static/js/main.f8529d57.chunk.js"
+  },
+  {
+    "revision": "cb0bad1c1e31adfb1992",
+    "url": "/nbd/static/js/runtime-main.12943e15.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nbd/precache-manifest.d1c382692c305bc836eea9705d851fe4.js"
+  "/nbd/precache-manifest.19a16a0a2fa0b9d6f1f4953e432c471c.js"
 );
 
 self.addEventListener('message', (event) => {
